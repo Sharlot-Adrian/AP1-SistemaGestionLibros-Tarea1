f47391818083a59f5ee74eb82344cc6edeb5b63c
Sharlot <sharlot_adrian@ucne.edu.do>
2026-09-12 17:26:57 -0400

    Se gestiono el contexto y la conexion con la base de datos

---
+++ RegistroLibros/Context/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroLibros.Models;
+
+namespace RegistroLibros.Context
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Libro> Libros { get; set; }
+
+    }
+
+    
+}
--- RegistroLibros/Models/Libro.cs
+++ RegistroLibros/Models/Libro.cs
@@ -1,6 +1,16 @@
-﻿namespace RegistroLibros.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroLibros.Models
 {
     public class Libro
     {
+        [Key]
+        public int LibroId { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public string Titulo { get; set; } = null!;
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public string Autor { get; set; } = null!;
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public DateTime AnoPublicacion { get; set; }
     }
 }
