4c9a1e93f56b12ecf0191633c4a190fc54d96259
Sharlot <sharlot_adrian@ucne.edu.do>
2026-09-12 20:41:33 -0400

    Se agregaron los metodos Existe e Insertar a LibroService.

--- RegistroLibros/Services/LibroService.cs
+++ RegistroLibros/Services/LibroService.cs
@@ -1,6 +1,24 @@
-﻿namespace RegistroLibros.Services
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroLibros.Context;
+using RegistroLibros.Models;
+
+namespace RegistroLibros.Services
 {
-    public class LibroService
+    public class LibroService(IDbContextFactory<Contexto> contextFactory) : Aplicada1.Core.IService<Libro,int>
     {
+        private async Task<bool> Existe(int libroId)
+        {
+            await using var contexto = await contextFactory.CreateDbContextAsync();
+            return await contexto.Libros.AnyAsync(p => p.LibroId == libroId);
+        }
+
+        private async Task<bool> Insertar(Libro libro)
+        {
+            await using var contexto = await contextFactory.CreateDbContextAsync();
+            contexto.Libros.Add(libro);
+            return await contexto.SaveChangesAsync() > 0;
+
+        }
+
     }
 }
