161937f19fbc9fc11604927a9db534ba6bec06e9
Sharlot <sharlot_adrian@ucne.edu.do>
2026-09-13 17:26:10 -0400

    Se inicio el proceso para implementar Toast.

---
+++ RegistroLibros/Extentors/ToastServiceExtentions.cs
@@ -0,0 +1,25 @@
+﻿namespace RegistroLibros.Extentors
+{
+    public static class ToastServiceExtentions
+    {
+        public static ToastMessage ShowToast(this ToastService toastService, ToastType toastType,
+        string title, string customMessage = null)
+        {
+            var message = new ToastMessage()
+            {
+                Type = toastType,
+                Title = title,
+                Message = customMessage ?? $"A las {DateTime.Now.ToString("hh:mm tt")}"
+            };
+
+            toastService.Notify(message);
+            return message;
+        }
+
+        public static ToastMessage ShowSuccess(this ToastService toastService, string customMessage = null,
+            string title = "Exito")
+        {
+            return toastService.ShowToast(ToastType.Success, title, customMessage);
+        }
+    }
+}
--- RegistroLibros/Program.cs
+++ RegistroLibros/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlite(ConStr));
 //Inyeccion del service
 builder.Services.AddScoped<LibroService>();
 
+builder.Services.AddBlazorBootstrap();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
