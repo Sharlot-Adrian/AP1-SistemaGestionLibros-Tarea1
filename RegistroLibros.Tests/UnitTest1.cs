a8618a45ba92d90f466aef8376e650c3a5291be8
Sharlot <sharlot_adrian@ucne.edu.do>
2026-09-14 21:12:26 -0400

    se agrego la branch practica en clase.

---
+++ RegistroLibros.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace RegistroLibros.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ RegistroLibros/Models/Partidas.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroLibros.Models
+{
+    public class Partidas
+    {
+        public int PartidaId { get; set;  }
+
+        public DateTime Fecha { get; set; }
+
+        public int Puntuacion { get; set; }
+
+        [ForeignKey("JugadorId")]
+        [InverseProperty("Partidas")]
+        public virtual Jugadores jugador { get; set; } = null!;
+
+    }
+}
