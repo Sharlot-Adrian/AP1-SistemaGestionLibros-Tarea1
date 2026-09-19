67c29f0a3fe57ed81bf69c0e24d6c0526790b472
Sharlot <sharlot_adrian@ucne.edu.do>
2026-09-19 15:19:37 -0400

    Se implemento el crear estudiante y se inicio a implementar el eliminar estudiante.

---
+++ RegistroLibros/Migrations/20260919164804_InsertarTablaEstudiantes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroLibros.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertarTablaEstudiantes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
